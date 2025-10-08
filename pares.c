9edf62306f51f4845fb0b4c1d7a5f2e95bba55a9
Luis Papazoglu <lpapazoglu@gmail.com>
2025-10-07 23:46:36 -0300

    agregamos funcion imprimir

--- pares.c
+++ pares.c
@@ -2,13 +2,14 @@
 
 //prototipos de funciones
 void carga(int v[], int n);
+void imprimir(int v[], int n);
 
 int main(){
 	
 	int vector[5];
 	carga(vector,5);
 	printf("fin del ejercicio");
-	//imprimir(vector,5);
+	imprimir(vector,5);
 	
 	
 	return 0;
@@ -19,5 +20,12 @@ void carga(int v[], int n){
 	for(i=0;i<n;i++){
 		printf("ingrese un valor: \n");
 		scanf("%i",&v[i]);
+	}
+}
+
+void imprimir(int v[], int n){
+	int i;
+	for(i=0;i<n;i++){
+		printf("valor: %i",v[i]);
 	}
 }
