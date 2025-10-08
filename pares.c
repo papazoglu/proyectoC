c15de8d0a9961ea58fcd3ce29f4d456f541a40e6
Luis Papazoglu <lpapazoglu@gmail.com>
2025-10-07 23:36:19 -0300

    primer commit

---
+++ pares.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+//prototipos de funciones
+void carga(int v[], int n);
+
+int main(){
+	
+	int vector[5];
+	carga(vector,5);
+	printf("fin del ejercicio");
+	//imprimir(vector,5);
+	
+	
+	return 0;
+}
+
+void carga(int v[], int n){
+	int i;
+	for(i=0;i<n;i++){
+		printf("ingrese un valor: \n");
+		scanf("%i",&v[i]);
+	}
+}
